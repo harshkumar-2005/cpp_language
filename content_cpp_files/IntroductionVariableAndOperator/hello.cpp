5caec7cce9efdb88bf0549b37dbe89bd1d1be7f1
Harsh Thakur <157991814+harshkumar-2005@users.noreply.github.com>
2024-12-19 21:09:37 +0530

    hello, world

---
+++ content_cpp_files/IntroductionVariableAndOperator/hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//using namespce std;
+int main()
+{
+    // This is a single line comment 
+
+    /*
+    This is a multi line comment
+    */
+
+   // First program in c++
+
+   std::cout<<"Hello, world";  // uncomment the line 2 and erase std::
+
+   // it is a just for standard library
+    
+    return 0;
+}
